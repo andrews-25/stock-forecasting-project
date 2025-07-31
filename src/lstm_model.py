9edf331fe52d1c5db84765ac70136d3928d22f53
Andrew Shea <andrewshea.2525@gmail.com>
2025-07-31 01:43:34 -0400

    Added device check and updated requirements

--- src/lstm_model.py
+++ src/lstm_model.py
@@ -1,15 +1,23 @@
 # === Imports ===
 import pandas as pd
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential             # type: ignore
+from tensorflow.keras.layers import LSTM, Dense, Dropout # type: ignore
 import matplotlib.pyplot as plt
-import os
 from data_handler import load_data
 
 
+
+#Device Check
 print("TensorFlow version:", tf.__version__)
-print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
-print("GPU Devices:", tf.config.list_physical_devices('GPU'))
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    print("Using device: GPU")
+else:
+    print("Using device: CPU")
+
