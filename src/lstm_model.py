80260593e58225afdd91d7b710a25c7aa8c7b5f3
andrew shea <andrewshea.2525@gmail.com>
2025-07-31 16:32:05 -0400

    Add data prep for LSTM Model

--- src/lstm_model.py
+++ src/lstm_model.py
@@ -6,7 +6,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow.keras.models import Sequential             # type: ignore
-from tensorflow.keras.layers import LSTM, Dense, Dropout # type: ignore
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Concatenate# type: ignore
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from data_handler import get_data
 
@@ -20,13 +21,29 @@ if gpus:
     print("Using device: GPU")
 else:
     print("Using device: CPU")
+    
+### Get Data and Normalize ###
 def normalize(df, features):
     scaler = MinMaxScaler()
     scaled = scaler.fit_transform(df[features])
     scaled_df = pd.DataFrame(scaled, columns = features, index = df.index)
     return  scaled_df, scaler 
-ticker = input("Enter Ticker: ")
+ticker = input("Enter Ticker: ").upper()
 df = get_data(ticker)
 features = ['Open', 'High', 'Low', 'Close', 'Volume']
 normalized_df, scaler = normalize(df, features)
-print(normalized_df.describe())
+
+window = 30
+input_seq = [] #LHCV inputs
+input_open = [] #current open
+output_close = [] #Todays Close
+
+for i in range (window, len(df)):
+    sequence = df[['Open', 'High', 'Low', 'Close', 'Volume']].iloc[i-window:i].values
+    current_open = df.iloc[i]['Open']
+    current_close = df.iloc[i]['Open']
+    
+    input_seq.append(sequence)
+    input_open.append([current_open])
+    output_close.append(current_close)
+
