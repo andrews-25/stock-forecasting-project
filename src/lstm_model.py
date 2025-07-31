0352c9be6d4d2572b1e15dfc6d222c559ee16579
Andrew Shea <andrewshea.2525@gmail.com>
2025-07-31 01:10:20 -0400

    changed file structure

---
+++ src/lstm_model.py
@@ -0,0 +1,15 @@
+# === Imports ===
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+import matplotlib.pyplot as plt
+import os
+from data_handler import load_data
+
+
+print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+print("GPU Devices:", tf.config.list_physical_devices('GPU'))
